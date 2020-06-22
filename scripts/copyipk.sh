b7303fbee438c70bc38371b1fc872623afb58bf3
kongfl888 <kongfl888@outlook.com>
2020-06-22 12:10:37 +0800

    Update copyipk

--- scripts/copyipk.sh
+++ scripts/copyipk.sh
@@ -19,14 +19,34 @@ if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*mwan3*.ipk" | g
 	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*mwan3*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack mwan3 ipk fail" >> ipklost.txt
 fi
 
-if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*luci-app-syncdial*.ipk" | grep "syncdial" -c` -gt 0 ]; then
-	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/luci-app-syncdial*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack syncdial ipk fail" >> ipklost.txt
+if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*syncdial*.ipk" | grep "syncdial" -c` -gt 0 ]; then
+	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*syncdial*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack syncdial ipk fail" >> ipklost.txt
+fi
+
+if [ `find friendlywrt-rk3328/friendlywrt/bin/targets/* -name "kmod-macvlan*.ipk" | grep "kmod-macvlan" -c` -gt 0 ]; then
+	mv -f friendlywrt-rk3328/friendlywrt/bin/targets/*/generic/packages/kmod-macvlan*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack kmod-macvlan ipk fail" >> ipklost.txt
 fi
 
 if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*luci-app-r2sflasher*.ipk" | grep "r2sflasher" -c` -gt 0 ]; then
-	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/luci-app-r2sflasher*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack r2sflasher ipk fail" >> ipklost.txt
+	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*r2sflasher*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack r2sflasher ipk fail" >> ipklost.txt
+fi
+
+if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*luci-app-autorewan*.ipk" | grep "autorewan" -c` -gt 0 ]; then
+	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*autorewan*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack autorewan ipk fail" >> ipklost.txt
+fi
+
+if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*smartdns*.ipk" | grep "smartdns" -c` -gt 0 ]; then
+	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*smartdns*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack smartdns ipk fail" >> ipklost.txt
+fi
+
+if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*koolproxyR*.ipk" | grep "koolproxyR" -c` -gt 0 ]; then
+	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*koolproxyR*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack koolproxyR ipk fail" >> ipklost.txt
 fi
 
 if [ `find ./r2srom/ipk/* -name "*.ipk" | grep ".ipk" -c` -eq 0 ]; then
     echo "1" > ./r2srom/ipk/noipk
+fi
+
+if [ -e ".current_config.mk" ]; then
+    cp -f .current_config.mk ./r2srom/
 fi
